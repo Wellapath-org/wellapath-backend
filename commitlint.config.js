67754ef0c27bd74012e2ae68f9b4c448e7bdc4f0
johnseyi <leojohnseyi@gmail.com>
2026-03-23 15:32:10 +0300

    chore: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'perf',
+        'ci',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
+  },
+};
